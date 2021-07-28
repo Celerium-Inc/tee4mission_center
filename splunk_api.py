e167d0b3b6488670389a3ba68114ed1497351c2d
Daniel Dye <dye.daniel@gmail.com>
2021-07-28 20:21:15 +0300

    debugging prints

--- splunk_api.py
+++ splunk_api.py
@@ -4,7 +4,7 @@ import requests
 
 
 def splunk_upload_stix(data, FLAGS):
-    get_response = requests.get(
+    before_get_response = requests.get(
         f'{FLAGS.splunk_host}servicesNS/nobody/Splunk_Security_Essentials/storage/collections/data/custom_content',
         auth=(FLAGS.splunk_username, FLAGS.splunk_password),
         verify=FLAGS.splunk_ssl_verify,
@@ -19,5 +19,17 @@ def splunk_upload_stix(data, FLAGS):
     )
     # expect http 201
     new_keys = post_response.json()
+    print(f'Created key: {new_keys["_key"]}')
+
+    if FLAGS.debug:
+        after_get_response = requests.get(
+            f'{FLAGS.splunk_host}servicesNS/nobody/Splunk_Security_Essentials/storage/collections/data/custom_content',
+            auth=(FLAGS.splunk_username, FLAGS.splunk_password),
+            verify=FLAGS.splunk_ssl_verify,
+        )
+        after_keys = [_['_key'] for _ in after_get_response.json()]
+        before_keys = [_['_key'] for _ in before_get_response.json()]
+        print(f'New keys added: {set(after_keys) - set(before_keys)}. N keys is now: {len(after_keys)}')
+
 
 
