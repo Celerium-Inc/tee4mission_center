f456f7e30633c0e1377b1e1566a3de162d372d80
Daniel Dye <dye.daniel@gmail.com>
2021-08-25 15:43:01 -0400

    revert (will add back via  PR)

--- mission_center_api.py
+++ mission_center_api.py
@@ -89,7 +89,7 @@ class MissionCenter:
                     f'{self.host}/api/jsonws/security.mbthread/get-group-threads?groupId={group_id}&start=-1&end=-1',
                     proxies={},
                     headers=self.headers,
-                    verify=self.FLAGS.mc_ssl_verify,
+                    verify=False,
                 )
                 threads.extend(result.json())
 
