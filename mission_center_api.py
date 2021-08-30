fd5bacf03a133d51a3d7f56bb3f62414d29e5b73
Daniel Dye <dye.daniel@gmail.com>
2021-08-30 18:16:44 -0400

    self.FLAGS -> self.flags

--- mission_center_api.py
+++ mission_center_api.py
@@ -12,13 +12,13 @@ from common import log
 
 
 class MissionCenter:
-    def __init__(self, FLAGS):
-        self.FLAGS = FLAGS
-        self.host = self.FLAGS.mc_host
-        self.username = self.FLAGS.mc_username
-        self.token = self.FLAGS.mc_api_key
+    def __init__(self, flags):
+        self.flags = flags
+        self.host = self.flags.mc_host
+        self.username = self.flags.mc_username
+        self.token = self.flags.mc_api_key
 
-        if self.FLAGS.mc_ssl_verify is False:
+        if self.flags.mc_ssl_verify is False:
             urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
         # data structures for tracking Mission Center Thread metadata; set by get_* methods
@@ -44,13 +44,13 @@ class MissionCenter:
         if self.jwt_token_expires < datetime.datetime.utcnow() + datetime.timedelta(seconds=5):
             self.refresh_token()
         # Request the URL
-        return requests.get(url, proxies={}, headers=self.headers, verify=self.FLAGS.mc_ssl_verify)
+        return requests.get(url, proxies={}, headers=self.headers, verify=self.flags.mc_ssl_verify)
 
     def get_current_user(self):
         """Set group_id list to identify each of the Compartments."""
         result = self._do_json_get_request(f'{self.host}/api/jsonws/security.currentuser/get-current-user')
         if result.status_code == 200:
-            if self.FLAGS.debug:
+            if self.flags.debug:
                 var_dump(result)
             # One and only one groupId for each Compartment
             self.group_ids = [_['groupId'] for _ in result.json().get('compartments', {})]
@@ -68,7 +68,7 @@ class MissionCenter:
                 f'{self.host}/api/jsonws/security.mbcategory/get-categories?groupId={group_id}&parentCategoryId=0&start=-1&end=-1',
                 proxies={},
                 headers=self.headers,
-                verify=self.FLAGS.mc_ssl_verify,
+                verify=self.flags.mc_ssl_verify,
             )
             category_records.extend(response.json())
 
@@ -91,7 +91,7 @@ class MissionCenter:
                     f'{self.host}/api/jsonws/security.mbthread/get-group-threads?groupId={group_id}&start=-1&end=-1',
                     proxies={},
                     headers=self.headers,
-                    verify=self.FLAGS.mc_ssl_verify,
+                    verify=self.flags.mc_ssl_verify,
                 )
                 threads.extend(result.json())
 
@@ -122,8 +122,8 @@ class MissionCenter:
         if getattr(self, 'group_ids', None) is None or len(self.group_ids) == 0:
             self.get_current_user()
 
-        if self.FLAGS.mc_include_categories:
-            group_ids_to_do = set(self.group_ids).intersection(set([int(_.split(';')[0]) for _ in self.FLAGS.mc_include_categories]))
+        if self.flags.mc_include_categories:
+            group_ids_to_do = set(self.group_ids).intersection(set([int(_.split(';')[0]) for _ in self.flags.mc_include_categories]))
         else:
             group_ids_to_do = self.group_ids
 
@@ -132,15 +132,15 @@ class MissionCenter:
             result = self._do_json_get_request(url)
             if result.status_code == 200:
                 all_threads = [_['threadId'] for _ in result.json()]
-                if self.FLAGS.mc_include_threads:
+                if self.flags.mc_include_threads:
                     # only save the intersection with the configured threadIds
-                    include_threads = [int(_) for _ in self.FLAGS.mc_include_threads]
-                    log(self.FLAGS, f'Only working on threads {include_threads} out of {all_threads} due to config flags.')
+                    include_threads = [int(_) for _ in self.flags.mc_include_threads]
+                    log(self.flags, f'Only working on threads {include_threads} out of {all_threads} due to config flags.')
                     self.thread_ids[group_id] = list(set(all_threads).intersection(set(include_threads)))
                 else:
                     self.thread_ids[group_id] = all_threads
             else:
-                log(self.FLAGS, f'Get Group Threads: Bad status code ({result.status_code}) for group_id: {group_id}.')
+                log(self.flags, f'Get Group Threads: Bad status code ({result.status_code}) for group_id: {group_id}.')
 
     def get_threat_extraction(self):
         """GET json/stix from Mission Center API and save to staging directory.
@@ -154,18 +154,18 @@ class MissionCenter:
         for group_id in self.thread_ids:
             for thread_id in self.thread_ids[group_id]:
                 missing_threat_extraction = False
-                for te_type in self.FLAGS.mc_te_types:
+                for te_type in self.flags.mc_te_types:
                     if missing_threat_extraction:
-                        log(self.FLAGS, f'Skipping the {te_type} download b/c the previous type failed')
+                        log(self.flags, f'Skipping the {te_type} download b/c the previous type failed')
                         continue
 
-                    log(self.FLAGS, f'Working on {group_id},{thread_id},{te_type}')
+                    log(self.flags, f'Working on {group_id},{thread_id},{te_type}')
 
                     staging_filename = f'./staging/{thread_id}.{te_type}'
                     complete_filename = f'./complete/{thread_id}.{te_type}'
 
                     if os.path.exists(complete_filename) or os.path.exists(staging_filename):
-                        log(self.FLAGS, f'{complete_filename} (or staging) exists. Skipping.')
+                        log(self.flags, f'{complete_filename} (or staging) exists. Skipping.')
                         continue
 
                     url = f'{self.host}/api/jsonws/security.mbthread/get-thread?&threadId={thread_id}&includePosts=false&includeTE=true&teType={te_type}&postsDesc=true&xssScrape=false'
@@ -175,5 +175,5 @@ class MissionCenter:
                         with open(staging_filename, 'w') as fh:
                             fh.write(threat_extraction_string)
                     else:
-                        log(self.FLAGS, f'Bad status code ({result.status_code} for thread_id: {thread_id}')
+                        log(self.flags, f'Bad status code ({result.status_code} for thread_id: {thread_id}')
                         missing_threat_extraction = True
