199bfd1254d0a1450063ea4d857d6ff1916184a9
Daniel Dye <dye.daniel@gmail.com>
2021-08-30 18:20:23 -0400

    FLAGS -> flags

--- splunk_api.py
+++ splunk_api.py
@@ -8,14 +8,14 @@ import requests
 from common import log
 
 
-def splunk_upload_kv(data, filepath, threads_df, FLAGS):
+def splunk_upload_kv(data, filepath, threads_df, flags):
     """Upload KeyValue Lookups to Splunk Enterprise Security.
 
     Args:
         b64str: base64 encoded stix XML
         filepath: path and file name
         threads_df: pandas dataframe with thread details
-        FLAGS: abseil flags
+        flags: abseil flags
 
     Returns:
         Boolean: Success/Failure
@@ -28,8 +28,8 @@ def splunk_upload_kv(data, filepath, threads_df, FLAGS):
     category_id = row['categoryId'].values[0]
     description = f'{subject};{base64.b64encode(bytes(row.to_json(), "utf-8")).decode("utf-8")}'
     # ToDo: find a way to create th "friendly" versions of these URLs:
-    # mc_url = f'{FLAGS.mc_host}/group/{compartment_friendly_url}/{configurable_page_name}/-/message_boards/message/{root_message_id}'
-    mc_url = f'{FLAGS.mc_host}/group/{group_id}/{category_id}/-/message_boards/message/{root_message_id}'
+    # mc_url = f'{flags.mc_host}/group/{compartment_friendly_url}/{configurable_page_name}/-/message_boards/message/{root_message_id}'
+    mc_url = f'{flags.mc_host}/group/{group_id}/{category_id}/-/message_boards/message/{root_message_id}'
 
     # ToDo: sort the observables by type and post the ones that use the same lookup_name together
     # ToDo: when should IP and Domain be combined into a single row?
@@ -60,52 +60,52 @@ def splunk_upload_kv(data, filepath, threads_df, FLAGS):
                 short_type = 'url'
                 lookup_name = 'http_intel'
             else:
-                log(FLAGS, 'new type encountered.  Debugging...')
+                log(flags, 'new type encountered.  Debugging...')
                 import pdb; pdb.set_trace()
 
         items.append({short_type: value, "description": description, "threat_key": mc_url})
 
         joined_items = ','.join([json.dumps(item) for item in items])
 
-        url = f'{FLAGS.splunk_host}services/data/threat_intel/item/{lookup_name}'
+        url = f'{flags.splunk_host}services/data/threat_intel/item/{lookup_name}'
 
         encoded = urllib.parse.quote_plus(f'[{joined_items}]')
         payload = f'item={encoded}'
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         post_response = requests.request("POST",
                                     url,
-                                    auth=(FLAGS.splunk_username, FLAGS.splunk_password),
+                                    auth=(flags.splunk_username, flags.splunk_password),
                                     headers=headers,
                                     data=payload,
-                                    verify = FLAGS.splunk_ssl_verify,
+                                    verify = flags.splunk_ssl_verify,
                                     )
 
-        log(FLAGS, post_response.__dict__)
+        log(flags, post_response.__dict__)
         if post_response.status_code >= 300:
-            log(FLAGS, 'Failed POST.  Debugging...')
+            log(flags, 'Failed POST.  Debugging...')
             """
             ['{"ip": "threatintel@htcc.secport.com","description": 
             """
             import pdb; pdb.set_trace()
 
-        log(FLAGS, post_response.json().get('message'))
+        log(flags, post_response.json().get('message'))
 
     return post_response.status_code < 300
 
 
-def splunk_es_upload_stix(b64str, filepath, FLAGS):
+def splunk_es_upload_stix(b64str, filepath, flags):
     """Upload stix file to Splunk Enterprise Security (deprecated).
 
     Args:
         b64str: base64 encoded stix XML
         filepath: path and file name
-        FLAGS: abseil flags
+        flags: abseil flags
 
     Returns:
         Boolean: Success/Failure
     """
 
-    url = f'{FLAGS.splunk_host}services/data/threat_intel/upload'
+    url = f'{flags.splunk_host}services/data/threat_intel/upload'
 
     _, filename = os.path.split(filepath)
     data = {
@@ -116,47 +116,47 @@ def splunk_es_upload_stix(b64str, filepath, FLAGS):
         'sinkhole': False,
     }
     post_response = requests.post(
-        url, json=data, auth=(FLAGS.splunk_username, FLAGS.splunk_password), verify=FLAGS.splunk_ssl_verify,
+        url, json=data, auth=(flags.splunk_username, flags.splunk_password), verify=flags.splunk_ssl_verify,
     )
-    log(FLAGS, post_response.json().get('message'))
+    log(flags, post_response.json().get('message'))
     return post_response.status_code < 300
 
 
-def splunk_upload_stix(data, FLAGS):
+def splunk_upload_stix(data, flags):
     """Upload stix file to Splunk Security Essentials.
 
     Args:
         data: JSON representation of the STIX object
-        FLAGS: abseil flags
+        flags: abseil flags
 
     Returns:
         Boolean: Success/Failure
     """
     before_get_response = requests.get(
-        f'{FLAGS.splunk_host}servicesNS/nobody/Splunk_Security_Essentials/storage/collections/data/custom_content',
-        auth=(FLAGS.splunk_username, FLAGS.splunk_password),
-        verify=FLAGS.splunk_ssl_verify,
+        f'{flags.splunk_host}servicesNS/nobody/Splunk_Security_Essentials/storage/collections/data/custom_content',
+        auth=(flags.splunk_username, flags.splunk_password),
+        verify=flags.splunk_ssl_verify,
     )
 
     post_response = requests.post(
-        f'{FLAGS.splunk_host}servicesNS/nobody/Splunk_Security_Essentials/storage/collections/data/custom_content',
+        f'{flags.splunk_host}servicesNS/nobody/Splunk_Security_Essentials/storage/collections/data/custom_content',
         data=json.dumps(data),
         headers={'Content-Type': 'application/json'},
-        auth=(FLAGS.splunk_username, FLAGS.splunk_password),
-        verify=FLAGS.splunk_ssl_verify,
+        auth=(flags.splunk_username, flags.splunk_password),
+        verify=flags.splunk_ssl_verify,
     )
     # expect http 201
     new_keys = post_response.json()
-    log(FLAGS, f'Created key: {new_keys["_key"]}')
+    log(flags, f'Created key: {new_keys["_key"]}')
 
-    if FLAGS.debug:
+    if flags.debug:
         after_get_response = requests.get(
-            f'{FLAGS.splunk_host}servicesNS/nobody/Splunk_Security_Essentials/storage/collections/data/custom_content',
-            auth=(FLAGS.splunk_username, FLAGS.splunk_password),
-            verify=FLAGS.splunk_ssl_verify,
+            f'{flags.splunk_host}servicesNS/nobody/Splunk_Security_Essentials/storage/collections/data/custom_content',
+            auth=(flags.splunk_username, flags.splunk_password),
+            verify=flags.splunk_ssl_verify,
         )
         after_keys = [_['_key'] for _ in after_get_response.json()]
         before_keys = [_['_key'] for _ in before_get_response.json()]
-        log(FLAGS, f'New keys added: {set(after_keys) - set(before_keys)}. N keys is now: {len(after_keys)}')
+        log(flags, f'New keys added: {set(after_keys) - set(before_keys)}. N keys is now: {len(after_keys)}')
 
     return post_response.status_code < 300
